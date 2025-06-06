4bbd2fc1caa8ea1d55e8d704a734275452b89cfe
nguyentrong04 <daonguyentrong.work@gmail.com>
2025-06-06 15:13:23 +0700

    add LinQ2SQL

--- DemoLinQ2SQL/Program.cs
+++ DemoLinQ2SQL/Program.cs
@@ -1,2 +1,121 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using DemoLinQ2SQL;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// khai báo chuỗi kết nối tới CSDL:
+string connectionString = "server=localhost;database=MyStore;uid=sa;password=12345";
+MyStoreDataContext context = new MyStoreDataContext(connectionString);
+//câu 1:Truy vấn toàn bộ danh mục
+var dsdm = context.Categories.ToList();
+Console.WriteLine("---Danh sách danh mục---");
+foreach (var d in dsdm)
+    Console.WriteLine(d.CategoryID + "\t" + d.CategoryName);
+
+// câu 2: Lấy thông tin chi tiết danh mục khi biết mã
+int madm = 7;
+Category cate = context.Categories.FirstOrDefault(c => c.CategoryID == madm);
+if (cate == null )
+{
+    Console.WriteLine("Không tìm thấy danh mục có mã = " + madm);
+}
+else
+{
+    Console.WriteLine("Tìm thấy danh mục có mã = " + madm);
+    Console.WriteLine(cate.CategoryID + "\t" + cate.CategoryName);
+}
+
+// câu 3 : dùng query syntax để truy vấn toàn bộ sản phẩm
+var dssp = from p in context.Products
+           select p;
+Console.WriteLine("---Danh sách sản phẩm:---");
+foreach (var p in dssp)
+{
+    Console.WriteLine(p.ProductID + "\t" + p.ProductName + "\t" + p.UnitPrice);
+}
+
+// câu 4: Dùng query syntax và anonymous type để lọc ra các sản phẩm nhưng chỉ lấy mã và đơn giá sản phẩm đồng thời sắp xếp giảm dần.
+
+var dssp4 = from p in context.Products
+            orderby p.UnitPrice descending
+            select new { p.ProductID, p.UnitPrice };
+Console.WriteLine("---Câu 4: Danh sách sản phẩm sắp xếp giảm dần theo đơn giá:---");
+foreach (var p in dssp4)
+{
+    Console.WriteLine(p.ProductID + "\t" + p.UnitPrice);
+}
+// câu 5: Sửa câu 4 theo extension method (Method syntax)
+var dssp5 = context.Products.OrderByDescending(p => p.UnitPrice)
+                            .Select(p => new { p.ProductID, p.UnitPrice });
+Console.WriteLine("---Câu 5: Danh sách sản phẩm sắp xếp giảm dần theo đơn giá (Method syntax):---");
+foreach (var p in dssp5)
+{
+    Console.WriteLine(p.ProductID + "\t" + p.UnitPrice);
+}
+// Lọc ra top 3 sản phẩm có đơn giá lớn nhất hệ thống yêu cầu dùng method syntax
+var dssp6 = context.Products.OrderByDescending(p => p.UnitPrice)
+                            .Take(3)
+                            .Select(p => new { p.ProductID, p.ProductName, p.UnitPrice });
+Console.WriteLine("---Câu 6: Danh sách top 3 sản phẩm có đơn giá lớn nhất:---");
+foreach (var p in dssp6)
+{
+    Console.WriteLine(p.ProductID + "\t" + p.UnitPrice + "\t\t" +  p.ProductName);
+}
+
+// câu 7: Sửa tên danh mục khi biết mã
+int madm7_edit = 3;
+Category cate_edit = context.Categories.FirstOrDefault(c => c.CategoryID == madm7_edit);
+if (cate_edit != null)
+{
+    cate_edit.CategoryName = "Hàng trôi nổi";
+    context.SubmitChanges();
+    Console.WriteLine("Đã sửa tên danh mục có mã = " + madm7_edit);
+}
+else
+{
+    Console.WriteLine("Không tìm thấy danh mục có mã = " + madm7_edit);
+}
+
+//câu 8: xóa danh mục khi biết mã   ==> XEM XÉT
+int madm_delete = 15;
+Category cate_remove = context.Categories.FirstOrDefault(c => c.CategoryID == madm_delete);
+if (cate_remove != null)
+{
+    context.Categories.DeleteOnSubmit(cate_remove);
+    context.SubmitChanges();
+    Console.WriteLine("Đã xóa danh mục có mã = " + madm_delete);
+}
+else
+{
+    Console.WriteLine("Không tìm thấy danh mục có mã = " + madm_delete);
+}
+
+// câu 9 : xóa các danh mục nếu không có bất kì sản phẩm
+// lưu ý: là xoas cùng 1 lúc nhiều danh mục, mà các dnah mục này không có chứa bất kỳ 1 sản phẩm nào
+
+var dsdm9 = context.Categories.Where(c => !c.Products.Any()).ToList();
+if (dsdm9.Count > 0)
+{
+    context.Categories.DeleteAllOnSubmit(dsdm9);
+    context.SubmitChanges();
+    Console.WriteLine("Đã xóa " + dsdm9.Count + " danh mục không có sản phẩm.");
+}
+else
+{
+    Console.WriteLine("Không có danh mục nào không có sản phẩm để xóa.");
+}
+
+// câu 10: thêm mới 1 danh mục
+Category c_new = new Category();
+c_new.CategoryName = "Hàng Lậu từ Trung Quốc";
+context.Categories.InsertOnSubmit(c_new);
+context.SubmitChanges();
+
+// câu 11:thêm mới nhiều danh mục
+List<Category> list = new List<Category>();
+list.Add(new Category { CategoryName = "Hàng gia dụng" });
+list.Add(new Category { CategoryName = "Hàng điện tử" });
+list.Add(new Category { CategoryName = "Hàng phụ kiện" });
+list.Add(new Category { CategoryName = "Hàng giả" });
+context.Categories.InsertAllOnSubmit(list);
+context.SubmitChanges();
---
+++ Lucy_SafesDataManagement/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using Lucy_SalesDataManagement;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// khai báo chuỗi kết nối tới CSDL:
+string connectionString = "server=localhost;database=Lucy_SalesData;uid=sa;password=12345";
+Lucy_SalesDataDataContext context = new Lucy_SalesDataDataContext(connectionString);
+
+// câu 1: lọc ra danh sách khách hàng
+var dskh = context.Customers.ToList();
+Console.WriteLine("---Danh sách khách hàng---");
+foreach (var kh in dskh)
+{
+    Console.WriteLine(kh.CustomerID + "\t" + kh.CompanyName + "\t" + kh.ContactName);
+}   
+
+// câu 2: tìm chi tiết thông tin khách hàng
+//khi biết mã khách hàng
+int makh = 10;
+Customer cust = context.Customers.FirstOrDefault(c => c.CustomerID == makh);
+if (cust != null)
+{
+    Console.WriteLine("Thông tin chi tiết khách hàng có mã " + makh + ":");
+    Console.WriteLine(cust.CustomerID + "\t" + cust.CompanyName + "\t" + cust.ContactName);
+}
+else
+{
+    Console.WriteLine("Không tìm thấy khách hàng có mã " + makh);
+}
+
+// câu 3: từ kết quả câu 2 : lọc ra danh sách các hóa đơn của khách hàng
+// các cột dữ liệu gồm: Mã hóa đơn + ngày hóa đơn
+
+var dsHoaDon = context.Orders
+    .Where(o => o.CustomerID == makh)
+    .Select(o => new { o.OrderID, o.OrderDate })
+    .ToList();
+Console.WriteLine("---Danh sách hóa đơn của khách hàng có mã " + makh + ":---");
+foreach (var hd in dsHoaDon)
+{
+    Console.WriteLine(hd.OrderID + "\t" + hd.OrderDate);
+}
+
+// câu 4: từ kết quả của câu 3 bổ sung thêm trị giá của mỗi hóa đơn
+if (cust != null)
+{
+    var dshd = context.Orders
+        .Where(o => o.CustomerID == makh)
+        .Select(o => new
+        {
+            o.OrderID,
+            o.OrderDate,
+            TotalValue = o.Order_Details.Sum(od => od.UnitPrice * od.Quantity * (1 -(decimal)od.Discount))
+        })
+        .ToList();
+    Console.WriteLine("---Danh sách hóa đơn của khách hàng có mã " + makh + " với trị giá:---");
+    foreach (var hd in dshd)
+    {
+        Console.WriteLine(hd.OrderID + "\t" + hd.OrderDate.ToString("dd/MM/yyyy") + "\t" + hd.TotalValue);
+    }
+}
