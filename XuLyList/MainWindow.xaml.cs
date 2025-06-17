6890adfea59b56b847c208a655bcc6d22babca1f
nguyentrong04 <daonguyentrong.work@gmail.com>
2025-06-17 14:26:18 +0700

    XulyList

---
+++ XuLyList/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XuLyList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<int> dsDuLieu = new List<int>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThem_Click(object sender, RoutedEventArgs e)
+        {
+            // x là giá trị muốn đưa vào
+            int x = int.Parse(txtGiaTri.Text);
+            // thêm x là list
+            dsDuLieu.Add(x);
+            HienThiDanhSach();
+            txtGiaTri.Text = ""; // xóa giá trị trong ô nhập
+
+        }
+
+        void HienThiDanhSach()
+        {
+            lstDuLieu.Items.Clear();
+            for (int i = 0; i < dsDuLieu.Count; i++)
+            {
+                int x = dsDuLieu[i];
+                lstDuLieu.Items.Add(x);
+            }
+        }
+
+        private void btnChen_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(txtGiaTriChen.Text);
+            int vt = int.Parse(txtViTriChen.Text);
+            dsDuLieu.Insert(vt, x);
+            HienThiDanhSach();
+            txtViTriChen.Text = "";
+            txtGiaTriChen.Text = "";
+        }
+
+        private void btnSapXepTang_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Sort();
+            
+            HienThiDanhSach();
+        }
+
+        private void btnSapXepGiam_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Sort();
+            dsDuLieu.Reverse();
+            HienThiDanhSach();
+        }
+
+        private void btnXoa1PhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstDuLieu.SelectedIndex == -1)
+            {
+                MessageBox.Show("Phải chọn phần tử mới xóa được", "Thông báo lỗi", MessageBoxButton.OK);
+                return;
+            }
+
+            // Xóa phần tử tại vị trí đang chọn:
+            dsDuLieu.RemoveAt(lstDuLieu.SelectedIndex);
+            HienThiDanhSach();
+        }
+
+        private void btnXoaNhieuPhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstDuLieu.SelectedIndex == -1)
+            {
+                MessageBox.Show("Phải chọn phần tử mới xóa được", "Thông báo lỗi", MessageBoxButton.OK);
+                return;
+            }
+
+            // Vòng lặp lấy các phần tử được chọn trên giao diện
+            while (lstDuLieu.SelectedItems.Count > 0)
+            {
+                // Lấy phần tử đầu tiên ra
+                int data = (int)lstDuLieu.SelectedItems[0];
+
+                // Xóa khỏi danh sách
+                dsDuLieu.Remove(data);
+
+                // Xóa dữ liệu trên giao diện
+                lstDuLieu.Items.Remove(data);
+            }
+        }
+
+        private void btnXoaToanBoPhanTu_Click(object sender, RoutedEventArgs e)
+        {
+            dsDuLieu.Clear(); // Xóa toàn bộ dữ liệu
+            HienThiDanhSach();
+        }
+    }
+}
